514247711244cf4049ba1b9f54b2cc4f44cdc158
Lukas Källström AIU21 <89090277+LukasKallstrom@users.noreply.github.com>
2021-09-07 14:08:16 +0200

    conitnued functions

---
+++ 6_Functions/geometry.py
@@ -0,0 +1,11 @@
+def triangle_area(base, height):
+    return base*height/2
+
+def triangle_circumferance(side1, side2, side3):
+    return sum(side1, side2, side3)
+
+def square_area(side1):
+    return side1**2
+
+def square_circumferance(side):
+    return side*4
---
+++ 6_Functions/main.py
@@ -0,0 +1,6 @@
+import geometry as geo
+
+print(geo.triangle_area(3,5))
+
+print(geo.square_area(10))
+
