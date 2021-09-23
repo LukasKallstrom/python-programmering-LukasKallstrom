e1b97ee27e84eb78d40696596a725eefb7cdac1d
Lukas Källström AIU21 <89090277+LukasKallstrom@users.noreply.github.com>
2021-09-23 11:39:42 +0200

    basic scatterplotting of org data+classified data

--- Labs/ClassifyPokemon/main.py
+++ Labs/ClassifyPokemon/main.py
@@ -39,38 +39,56 @@ with open("Labs/ClassifyPokemon/files/pichu.txt", "r") as f_pichu, open("Labs/Cl
     remove_clutter(pikachu_data, pikachu_x, pikachu_y)
     remove_clutter(test_points, test_points_x, test_points_y)
 
-    plt.scatter(pichu_x, pichu_y, c="r" )
-    plt.scatter(pikachu_x, pikachu_y, c="b")
-    #plt.show()
-    #plt.legend()
+    plt.scatter(pichu_x, pichu_y, c="lime")
+    plt.scatter(pikachu_x, pikachu_y, c="magenta")
 
     point = (25, 35)
-    
+    classified_pichu =  []
+    classified_pikachu = []
     avrg_pichu = 0
     avrg_pikachu = 0
     
-    distances_pichu = []
-    distances_pikachu = []
-    
-    for i in range(50):
-        distances_pichu.append(math.sqrt((pichu_x[i] - point[0])**2 + (pichu_y[i] - point[1])**2))
-    for i in range(50):
-        distances_pikachu.append(math.sqrt((pikachu_x[i] - point[0])**2 + (pikachu_y[i] - point[1])**2))
-    distances_pichu.sort()
-    distances_pikachu.sort()
+        
+    def classify_five_points():
+        for j in range(4):
+            distances_pichu = []
+            distances_pikachu = []
+            for i in range(50):
+                distances_pichu.append(math.sqrt((pichu_x[i] - test_points_x[j])**2 + (pichu_y[i] - test_points_y[j])**2))
+            for i in range(50):
+                distances_pikachu.append(math.sqrt((pikachu_x[i] - test_points_x[j])**2 + (pikachu_y[i] - test_points_y[j])**2))
 
-    avrg_pichu = sum(distances_pichu[0:5]) / len(distances_pichu[0:5])
-    avrg_pikachu = sum(distances_pikachu[0:5]) / len(distances_pikachu[0:5])
+            distances_pichu.sort()
+            distances_pikachu.sort()
 
-    print(f"Pichu shortest distances: {distances_pichu[0:5]}")
-    print(f"Pikachu shortest distances: {distances_pikachu[0:5]}")
-    print(f"Pichu avarage: {avrg_pichu}")
-    print(f"Pikachu avarage: {avrg_pikachu}")
+            avrg_pichu = sum(distances_pichu[0:5]) / len(distances_pichu[0:5])
+            avrg_pikachu = sum(distances_pikachu[0:5]) / len(distances_pikachu[0:5])
 
-    if avrg_pichu < avrg_pikachu:
-        print(f"Data point belongs to Pichu")
-    else:
-        print(f"Data point belongs to Pikachu")
+            if avrg_pichu < avrg_pikachu:
+                print(f"Sample with (width, height): ({test_points_x[j]:.0f}, {test_points_y[j]:.0f}) classified as Pichu")
+            else:
+                print(f"Sample with (width, height): ({test_points_x[j]:.0f}, {test_points_y[j]:.0f}) classified as Pikachu")
 
 
+    def classify_closest_point():        
+        for j in range(4):
+            distances_pichu = []
+            distances_pikachu = []
+            for i in range(50):
+                distances_pichu.append(math.sqrt((pichu_x[i] - test_points_x[j])**2 + (pichu_y[i] - test_points_y[j])**2))
+            for i in range(50):
+                distances_pikachu.append(math.sqrt((pikachu_x[i] - test_points_x[j])**2 + (pikachu_y[i] - test_points_y[j])**2))
+
+            distances_pichu.sort()
+            distances_pikachu.sort()
+
+            if distances_pichu[0] < distances_pikachu[0]:
+                print(f"Sample with (width, height): ({test_points_x[j]:.0f}, {test_points_y[j]:.0f}) classified as Pichu")
+                plt.scatter(test_points_x[j],test_points_y[j], c="green", s=100)
+            else:
+                print(f"Sample with (width, height): ({test_points_x[j]:.0f}, {test_points_y[j]:.0f}) classified as Pikachu")
+                plt.scatter(test_points_x[j],test_points_y[j], c="firebrick", s=100)
+    classify_closest_point()
+    plt.legend(["Pichu", "Pikachu", "Classified as Pikachu", "Classified as Pichu"])
+    plt.show()
     
