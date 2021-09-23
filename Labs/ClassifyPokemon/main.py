c6af0d30b95c8fb9a541fd1708813d2268b2d17f
Lukas Källström AIU21 <89090277+LukasKallstrom@users.noreply.github.com>
2021-09-23 10:53:19 +0200

    basic comparison test working

--- Labs/ClassifyPokemon/main.py
+++ Labs/ClassifyPokemon/main.py
@@ -1,9 +1,8 @@
 from matplotlib import colors
-import matplotlib
 import matplotlib.pyplot as plt
-import numpy as np
+import math
 
-def remove_clutter(data:str, x:list, y:list) -> None:
+def remove_clutter(data:str, x_list:list, y_list:list) -> None:
         temp_coor = []
         temp = ""
 
@@ -13,30 +12,65 @@ def remove_clutter(data:str, x:list, y:list) -> None:
             temp = temp.replace(")","")
             temp = temp.replace(",","")
             temp_coor = temp.split()
-            x.append(float(temp_coor[0]))
-            y.append(float(temp_coor[1]))
+            x_list.append(float(temp_coor[0]))
+            y_list.append(float(temp_coor[1]))
 
-def plot_data(data:str,color:str):
+def plot_data(data:str, color:str):
     remove_clutter(data)
     plt.scatter
 
 
-with open("Labs/ClassifyPokemon/files/pichu.txt", "r") as f_pichu, open("Labs/ClassifyPokemon/files/pikachu.txt", "r") as f_pikachu:
+with open("Labs/ClassifyPokemon/files/pichu.txt", "r") as f_pichu, open("Labs/ClassifyPokemon/files/pikachu.txt", "r") as f_pikachu, open("Labs/ClassifyPokemon/files/test_points.txt", "r") as f_testpoints:
 
     pichu_data = f_pichu.readlines()
     pikachu_data = f_pikachu.readlines()
+    test_points = f_testpoints.readlines()
     pichu_data.pop(0)
     pikachu_data.pop(0)
     pikachu_x = []
     pikachu_y = []
     pichu_x = []
     pichu_y = []
+    test_points_x = []
+    test_points_y = []
     
     
     remove_clutter(pichu_data, pichu_x, pichu_y)
     remove_clutter(pikachu_data, pikachu_x, pikachu_y)
+    remove_clutter(test_points, test_points_x, test_points_y)
 
     plt.scatter(pichu_x, pichu_y, c="r" )
     plt.scatter(pikachu_x, pikachu_y, c="b")
-    plt.show()
-    plt.legend()
+    #plt.show()
+    #plt.legend()
+
+    point = (25, 35)
+    
+    avrg_pichu = 0
+    avrg_pikachu = 0
+    
+    distances_pichu = []
+    distances_pikachu = []
+    
+    for i in range(50):
+        distances_pichu.append(math.sqrt((pichu_x[i] - point[0])**2 + (pichu_y[i] - point[1])**2))
+    for i in range(50):
+        distances_pikachu.append(math.sqrt((pikachu_x[i] - point[0])**2 + (pikachu_y[i] - point[1])**2))
+    distances_pichu.sort()
+    distances_pikachu.sort()
+
+    avrg_pichu = sum(distances_pichu[0:5]) / len(distances_pichu[0:5])
+    avrg_pikachu = sum(distances_pikachu[0:5]) / len(distances_pikachu[0:5])
+
+    print(f"Pichu shortest distances: {distances_pichu[0:5]}")
+    print(f"Pikachu shortest distances: {distances_pikachu[0:5]}")
+    print(f"Pichu avarage: {avrg_pichu}")
+    print(f"Pikachu avarage: {avrg_pikachu}")
+
+    if avrg_pichu < avrg_pikachu:
+        print(f"Data point belongs to Pichu")
+    else:
+        print(f"Data point belongs to Pikachu")
+
+
+    
