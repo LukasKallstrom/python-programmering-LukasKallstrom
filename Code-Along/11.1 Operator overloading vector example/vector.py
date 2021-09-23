9f9de10755b5786ade4301b536cfb65e99c937d9
Lukas Källström AIU21 <89090277+LukasKallstrom@users.noreply.github.com>
2021-09-23 10:52:12 +0200

    start of vector class

--- Code-Along/11.1 Operator overloading vector example/vector.py
+++ Code-Along/11.1 Operator overloading vector example/vector.py
@@ -1,1 +1,9 @@
-2
+class Vector:
+    """ A class to represent a Euclidean vector with magnitude and direction"""
+
+    def __init__(self, *numbers) -> None:
+        print(numbers)
+
+v1 = Vector(1,2)
+
+# v1 = (1,1), v2 = (1,1,3,5,67,13,23,3,8)
