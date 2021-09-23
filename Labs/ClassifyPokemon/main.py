fe6fd39eb0f46d7d6dd808c6e4ccd29f8e7990f3
Lukas Källström AIU21 <89090277+LukasKallstrom@users.noreply.github.com>
2021-09-23 09:00:20 +0200

    changes

--- Labs/ClassifyPokemon/main.py
+++ Labs/ClassifyPokemon/main.py
@@ -1,9 +1,42 @@
+from matplotlib import colors
+import matplotlib
 import matplotlib.pyplot as plt
-with open("ClassifyPokemon/files/pichu.txt", "r") as f_pichu, open("ClassifyPokemon/files/pikachu.txt", "r") as f_pikachu:
+import numpy as np
+
+def remove_clutter(data:str, x:list, y:list) -> None:
+        temp_coor = []
+        temp = ""
+
+        for line in data:
+            temp = line
+            temp = temp.replace("(","")
+            temp = temp.replace(")","")
+            temp = temp.replace(",","")
+            temp_coor = temp.split()
+            x.append(float(temp_coor[0]))
+            y.append(float(temp_coor[1]))
+
+def plot_data(data:str,color:str):
+    remove_clutter(data)
+    plt.scatter
+
+
+with open("Labs/ClassifyPokemon/files/pichu.txt", "r") as f_pichu, open("Labs/ClassifyPokemon/files/pikachu.txt", "r") as f_pikachu:
 
     pichu_data = f_pichu.readlines()
     pikachu_data = f_pikachu.readlines()
     pichu_data.pop(0)
     pikachu_data.pop(0)
-    plt.show()
+    pikachu_x = []
+    pikachu_y = []
+    pichu_x = []
+    pichu_y = []
+    
+    
+    remove_clutter(pichu_data, pichu_x, pichu_y)
+    remove_clutter(pikachu_data, pikachu_x, pikachu_y)
 
+    plt.scatter(pichu_x, pichu_y, c="r" )
+    plt.scatter(pikachu_x, pikachu_y, c="b")
+    plt.show()
+    plt.legend()
